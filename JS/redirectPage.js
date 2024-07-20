01c16fc7fa1b1a7bb1636558830b28e7e2b94f29
leeyang19970916 <91140699+leeyang19970916@users.noreply.github.com>
2024-07-21 00:16:12 +0800

    fix: sidebar /route are all completed

--- JS/redirectPage.js
+++ JS/redirectPage.js
@@ -1,10 +1,15 @@
 import basePath from "./domain.js";
+import PageList from "./listForPage.js";
 
 const redirectPage = (dom) => {
   dom.addEventListener("click", (event) => {
     const e = event.target;
     const pageID = e.getAttribute("id");
-    if (pageID === "Index") {
+    if (pageID==="sideBar") {
+      return
+    }
+    const isExist=PageList.findIndex(i=>i.id===pageID)
+    if (isExist===-1 ) {
       window.location.href = `${basePath}/`;
       return;
     }
