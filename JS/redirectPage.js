18021a5aae00c7327f9418b93921b6f02e8e558c
leeyang19970916 <91140699+leeyang19970916@users.noreply.github.com>
2024-07-20 21:58:46 +0800

    feat: update UI and route index

--- JS/redirectPage.js
+++ JS/redirectPage.js
@@ -2,6 +2,11 @@ const redirectPage = (dom) => {
   dom.addEventListener("click", () => {
     const e = event.target;
     const pageID = e.getAttribute("id");
+    if (pageID==="Index") {
+      window.location.href = '/';
+      ;
+      return
+    }
     window.open(`../pages/sample.html?id=${pageID}`);
   });
 };
--- JS/sideBarUI.js
+++ JS/sideBarUI.js
@@ -1,10 +1,11 @@
 import PageList from "./listForPage.js";
 import redirectPage from "./redirectPage.js";
 
+const sideBarList=[{id:"Index",name:"Index"},...PageList]
 const SideBar=(urlID)=>{
 const siderBarDom=document.querySelector("nav#sideBar")
 let str=''
-PageList.forEach(item=>{
+sideBarList.forEach(item=>{
     let isActive=item.id===urlID ? 'active' : ''
     str+=`<div id=${item.id} class='sidebarItem ${isActive}'>${item.name}</div>`
 })
