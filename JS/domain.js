d67c385b23963e95ac9066730511532a9748cc41
leeyang19970916 <91140699+leeyang19970916@users.noreply.github.com>
2024-07-21 00:23:34 +0800

    del remark

--- JS/domain.js
+++ JS/domain.js
@@ -1,7 +1,6 @@
 const DOMAIN_HOST = `https://leeyang19970916.github.io/Git_tutorial`;
 const LOCAL_HOST = ``;
 
-// 判斷當前是否在本地運行
 const isLocal = window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost';
 const basePath = isLocal ? LOCAL_HOST : DOMAIN_HOST;
 
